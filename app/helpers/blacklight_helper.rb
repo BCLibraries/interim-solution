be67326c11a19b19b0e7c62ea400e2026404a19b
Benjamin Florin <benjamin.florin@gmail.com>
2016-02-03 19:34:28 -0500

    Catch more obvious multi-image links

--- app/helpers/blacklight_helper.rb
+++ app/helpers/blacklight_helper.rb
@@ -4,25 +4,26 @@ module BlacklightHelper
   # Are there more images than just the thumbnail?
   def more_images?(item_id_array, physical_description)
 
+    return_val = false
+
     # If there are item identifiers, check for a range
     if item_id_array.length > 0
       item_id_strings = item_id_array[0].split('-')
 
-      if item_id_strings.length != 2
-        false
+      if item_id_strings.length == 2
+        return_val = true
       end
 
       if item_id_strings[1] and item_id_strings[0]
-        item_id_strings[1] != item_id_strings[0]
+        return_val = item_id_strings[1] != item_id_strings[0]
+      end
+    else
+      if physical_description.length > 0
+        return_val = physical_description[0].include?('surrogates') or physical_description[0].include?('images')
       end
     end
 
-    # If not, check the physical description for 'surrogates'
-    if physical_description.length > 0
-      physical_description[0].include? 'surrogates'
-    else
-      false
-    end
+    return_val
   end
 
   def presenter_class
