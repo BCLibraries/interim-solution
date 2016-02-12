7375acde81c9decfe36897697e73891f3ca94ad3
Benjamin Florin <benjamin.florin@gmail.com>
2016-02-12 17:49:27 -0500

    Rewrote home image grid in Bootstrap

---
+++ app/helpers/collections_helper.rb
@@ -0,0 +1,161 @@
+module CollectionsHelper
+
+  def highlighted_collections
+    return [
+        {
+            facet_val: "Abstract+of+a+cruise+in+the+frigate+United+States+under+the+command+of+Commandore+Thos+ap+Catesby+Jones%2C+James+Armstrong+esq.+Captain+in+the+South+Pacific",
+            title: "Abstract of a cruise in the frigate United State",
+            img: "http://dcollections.bc.edu/webclient/DeliveryManager?pid=508255"
+        },
+        {
+            facet_val: "Bobbie+Hanvey+Photographic+Archives",
+            title: "Bobbie Hanvey Photographic Archives",
+            img: "http://dcollections.bc.edu/webclient/DeliveryManager?pid=508255"
+        },
+        {
+            facet_val: "Book+of+hours",
+            title: "Book of Hours",
+            img: "http://dcollections.bc.edu/webclient/DeliveryManager?pid=508255"
+        },        {
+            facet_val: "Boston+College+students+and+campus+life+photographs",
+            title: "Boston College students and campus life photographs",
+            img: "http://dcollections.bc.edu/webclient/DeliveryManager?pid=508255"
+        },
+        {
+            facet_val: "Boston+College+athletic+photographs",
+            title: "Boston College athletic photographs",
+            img: "http://dcollections.bc.edu/webclient/DeliveryManager?pid=508255"
+        },
+
+        {
+            facet_val: "Boston+College+building+and+campus+images",
+            title: "Boston College building and campus images",
+            img: "http://dcollections.bc.edu/webclient/DeliveryManager?pid=508255"
+        },
+
+        {
+            facet_val: "Boston+College+Commencement+photographs",
+            title: "Boston College Commencement photographs",
+            img: "http://dcollections.bc.edu/webclient/DeliveryManager?pid=508255"
+        },
+
+        {
+            facet_val: "Boston+College+Evening+College+photographs+%28early+years%29",
+            title: "Boston College Evening College photographs (early years)",
+            img: "http://dcollections.bc.edu/webclient/DeliveryManager?pid=508255"
+        },
+
+        {
+            facet_val: "Boston+College+faculty+and+staff+photographs",
+            title: "Boston College faculty and staff photographs",
+            img: "http://dcollections.bc.edu/webclient/DeliveryManager?pid=508255"
+        },
+        {
+            facet_val: "Boston+College+special+guests+and+events+photographs",
+            title: "Boston College special guests and events photographs",
+            img: "http://dcollections.bc.edu/webclient/DeliveryManager?pid=508255"
+        },
+        {
+            facet_val: "Boston+College+theater+productions",
+            title: "Boston College theater productions",
+            img: "http://dcollections.bc.edu/webclient/DeliveryManager?pid=508255"
+        },
+
+        {
+            facet_val: "Charter+of+Boston+College+and+amendments",
+            title: "Charter of Boston College and amendments",
+            img: "http://dcollections.bc.edu/webclient/DeliveryManager?pid=508255"
+        },
+
+        {
+            facet_val: "%5BConnolly+book+of+hours%5D",
+            title: "[Connolly book of hours]",
+            img: "http://dcollections.bc.edu/webclient/DeliveryManager?pid=508255"
+        },
+
+        {
+            facet_val: "Elizabeth+Capell+recipe+book",
+            title: "Elizabeth Capell recipe book",
+            img: "http://dcollections.bc.edu/webclient/DeliveryManager?pid=508255"
+        },
+
+        {
+            facet_val: "Elizabeth+Hayward+collection+of+Ursuline+Academy+materials",
+            title: "Elizabeth Hayward collection of Ursuline Academy materials",
+            img: "http://dcollections.bc.edu/webclient/DeliveryManager?pid=508255"
+        },
+
+        {
+            facet_val: "Francis+Borgia+letter%2C+1557+November+1",
+            title: "Francis Borgia letter, 1557 November 1",
+            img: "http://dcollections.bc.edu/webclient/DeliveryManager?pid=508255"
+        },
+
+        {
+            facet_val: "Francis+Xavier+letter%2C+1552+January+31",
+            title: "Francis Xavier letter, 1552 January 31",
+            img: "http://dcollections.bc.edu/webclient/DeliveryManager?pid=508255"
+        },
+
+        {
+            facet_val: "Franciscan+antiphoner+%28sanctorale%29+with+sequences%2C+circa+1300-1350",
+            title: "Franciscan antiphoner sanctorale with sequences irca 1300-1350",
+            img: "http://dcollections.bc.edu/webclient/DeliveryManager?pid=508255"
+        },
+
+        {
+            facet_val: "James+Jeffrey+Roche+letters",
+            title: "James Jeffrey Roche letters",
+            img: "http://dcollections.bc.edu/webclient/DeliveryManager?pid=508255"
+        },
+
+        {
+            facet_val: "John+Donnelly+%26+Sons+records",
+            title: "John Donnelly &amp; Sons records",
+            img: "http://dcollections.bc.edu/webclient/DeliveryManager?pid=508255"
+        },
+
+        {
+            facet_val: "Letter%2C+1798+June+8",
+            title: "Letter, 1798 June 8",
+            img: "http://dcollections.bc.edu/webclient/DeliveryManager?pid=508255"
+        },
+
+        {
+            facet_val: "Mariana+Islands+letters",
+            title: "Mariana Islands letters",
+            img: "http://dcollections.bc.edu/webclient/DeliveryManager?pid=508255"
+        },
+
+        {
+            facet_val: "Michael+H.+Leary+letters",
+            title: "Michael H. Leary letters",
+            img: "http://dcollections.bc.edu/webclient/DeliveryManager?pid=508255"
+        },
+
+        {
+            facet_val: "Robert+Bellarmine+letter%2C+1598+February+5",
+            title: "Robert Bellarmine letter, 1598 February 5",
+            img: "http://dcollections.bc.edu/webclient/DeliveryManager?pid=508255"
+        },
+
+        {
+            facet_val: "The+Robert+E.+Brooker+III+Collection+of+American+Legal+and+Land+Use+Documents%2C+1716-1930",
+            title: "The Robert E. Brooker III Collection of American Legal and Land Use Documents, 1716-1930",
+            img: "http://dcollections.bc.edu/webclient/DeliveryManager?pid=508255"
+        },
+
+        {
+            facet_val: "Thomas+D.+Craven+papers",
+            title: "Thomas D. Craven papers",
+            img: "http://dcollections.bc.edu/webclient/DeliveryManager?pid=508255"
+        },
+
+        {
+            facet_val: "William+Butler+Yeats+notebooks+and+manuscripts",
+            title: "William Butler Yeats notebooks and manuscripts",
+            img: "http://dcollections.bc.edu/webclient/DeliveryManager?pid=508255"
+        }
+    ];
+  end
+end
