86229a7dc162d3c327cccf057810ecbbdbb6334a
Benjamin Florin <benjamin.florin@gmail.com>
2016-02-18 15:42:32 -0500

    Added Google Analytics

---
+++ app/assets/javascripts/analytics.js
@@ -0,0 +1,52 @@
+// Converted from Jonathan Wolfe's GA Coffeescript code at http://reed.github.io/turbolinks-compatibility/google_analytics.html
+
+this.GoogleAnalytics = (function() {
+    function GoogleAnalytics() {}
+
+    GoogleAnalytics.load = function() {
+        var firstScript, ga;
+        window._gaq = [];
+        window._gaq.push(["_setAccount", GoogleAnalytics.analyticsId()]);
+        ga = document.createElement("script");
+        ga.type = "text/javascript";
+        ga.async = true;
+        ga.src = ("https:" === document.location.protocol ? "https://ssl" : "http://www") + ".google-analytics.com/ga.js";
+        firstScript = document.getElementsByTagName("script")[0];
+        firstScript.parentNode.insertBefore(ga, firstScript);
+        if (typeof Turbolinks !== 'undefined' && Turbolinks.supported) {
+            return document.addEventListener("page:change", (function() {
+                return GoogleAnalytics.trackPageview();
+            }), true);
+        } else {
+            return GoogleAnalytics.trackPageview();
+        }
+    };
+
+    GoogleAnalytics.trackPageview = function(url) {
+        if (!GoogleAnalytics.isLocalRequest()) {
+            if (url) {
+                window._gaq.push(["_trackPageview", url]);
+            } else {
+                window._gaq.push(["_trackPageview"]);
+            }
+            return window._gaq.push(["_trackPageLoadTime"]);
+        }
+    };
+
+    GoogleAnalytics.isLocalRequest = function() {
+        return GoogleAnalytics.documentDomainIncludes("local") || GoogleAnalytics.documentDomainIncludes("162");
+    };
+
+    GoogleAnalytics.documentDomainIncludes = function(str) {
+        return document.domain.indexOf(str) !== -1;
+    };
+
+    GoogleAnalytics.analyticsId = function() {
+        return 'UA-3008279-18';
+    };
+
+    return GoogleAnalytics;
+
+})();
+
+GoogleAnalytics.load();
