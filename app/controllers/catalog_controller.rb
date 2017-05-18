f34374eda07d321b5177bf34a27985c6bd6a95f8
Adam Jazairi <adamjazairi@gmail.com>
2017-05-18 16:40:50 -0400

    Index and show non-primary authors, photographers, and creators

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -1,24 +1,24 @@
 # -*- encoding : utf-8 -*-
-class CatalogController < ApplicationController  
+class CatalogController < ApplicationController
   include Blacklight::Marc::Catalog
 
   include Blacklight::Catalog
 
   configure_blacklight do |config|
     ## Default parameters to send to solr for all search-like requests. See also SearchBuilder#processed_parameters
-    config.default_solr_params = { 
+    config.default_solr_params = {
       :qt => 'search',
       :rows => 10,
-      :"hl.fl" => 'mods_titleInfo_usage_primary_title_ms' 
+      :"hl.fl" => 'mods_titleInfo_usage_primary_title_ms'
 
     }
- 
 
-    config.index.thumbnail_field = :mods_location_url_access_preview_ms    
+
+    config.index.thumbnail_field = :mods_location_url_access_preview_ms
 
     # solr path which will be added to solr base url before the other solr params.
-    #config.solr_path = 'select' 
-    
+    #config.solr_path = 'select'
+
     # items to show per page, each number in the array represent another option to choose from.
     #config.per_page = [10,20,50,100]
 
@@ -30,14 +30,14 @@ class CatalogController < ApplicationController
     #  ## These are hard-coded in the blacklight 'document' requestHandler
     #  # :fl => '*',
     #  # :rows => 1
-    #  # :q => '{!raw f=id v=$id}' 
+    #  # :q => '{!raw f=id v=$id}'
     #}
 
     # solr field configuration for search results/index views
     config.index.title_field = 'mods_titleInfo_usage_primary_title_ms'
     #config.index.display_type_field = 'format'
     config.index.display_type_field = 'mods_format_normalized_ms'
-    
+
 
     # solr field configuration for document/show views
     #config.show.title_field = 'title_display'
@@ -53,17 +53,17 @@ class CatalogController < ApplicationController
     # * If left unset, then all facet values returned by solr will be displayed.
     # * If set to an integer, then "f.somefield.facet.limit" will be added to
     # solr request, with actual solr request being +1 your configured limit --
-    # you configure the number of items you actually want _displayed_ in a page.    
+    # you configure the number of items you actually want _displayed_ in a page.
     # * If set to 'true', then no additional parameters will be sent to solr,
     # but any 'sniffed' request limit parameters will be used for paging, with
-    # paging at requested limit -1. Can sniff from facet.limit or 
+    # paging at requested limit -1. Can sniff from facet.limit or
     # f.specific_field.facet.limit solr request params. This 'true' config
     # can be used if you set limits in :default_solr_params, or as defaults
     # on the solr side in the request handler itself. Request handler defaults
     # sniffing requires solr requests to be made with "echoParams=all", for
-    # app code to actually have it echo'd back to see it.  
+    # app code to actually have it echo'd back to see it.
     #
-    # :show may be set to false if you don't want the facet to be drawn in the 
+    # :show may be set to false if you don't want the facet to be drawn in the
     # facet bar
     config.add_facet_field 'mods_relatedItem_type_host_collectionFacet_ms', :label => 'Collection/Series', limit: 10
     config.add_facet_field 'mods_relatedItem_type_host_collection_ms', :label => 'Collection/Series', :show => false
@@ -90,8 +90,8 @@ class CatalogController < ApplicationController
     config.add_facet_fields_to_solr_request!
 
     # solr fields to be displayed in the index (search results) view
-    #   The ordering of the field names is the order of the display 
-    
+    #   The ordering of the field names is the order of the display
+
     #config.add_index_field 'title_display', :label => 'Title'
     #config.add_index_field 'title_vern_display', :label => 'Title'
     #config.add_index_field 'author_display', :label => 'Author'
@@ -105,6 +105,9 @@ class CatalogController < ApplicationController
     config.add_index_field 'mods_name_usage_primary_displayForm_aut_ms', :label => 'Author'
     config.add_index_field 'mods_name_usage_primary_displayForm_cre_ms', :label => 'Creator'
     config.add_index_field 'mods_name_usage_primary_displayForm_pht_ms', :label => 'Photographer'
+    config.add_index_field 'mods_name_displayForm_aut_ms', :label => 'Additional Author(s)'
+    config.add_index_field 'mods_name_displayForm_cre_ms', :label => 'Additional Creator(s)'
+    config.add_index_field 'mods_name_displayForm_pht_ms', :label => 'Additional Photographer(s)'
     config.add_index_field 'mods_name_displayForm_ctb_ms', :label => 'Contributor'
     config.add_index_field 'mods_date_display_ms', :label => 'Date'
     config.add_index_field 'mods_physicalDescription_extent_ms', :label => 'Physical Description'
@@ -114,7 +117,7 @@ class CatalogController < ApplicationController
 
 
     # solr fields to be displayed in the show (single result) view
-    #   The ordering of the field names is the order of the display 
+    #   The ordering of the field names is the order of the display
     #config.add_show_field 'title_display', :label => 'Title'
     #config.add_show_field 'title_vern_display', :label => 'Title'
     #config.add_show_field 'subtitle_display', :label => 'Subtitle'
@@ -138,6 +141,9 @@ class CatalogController < ApplicationController
     config.add_show_field 'mods_name_usage_primary_displayForm_aut_ms', :label => 'Author', :link_to_search => 'mods_name_usage_primary_displayForm_aut_ms'
     config.add_show_field 'mods_name_usage_primary_displayForm_cre_ms', :label => 'Creator', :link_to_search => 'mods_name_usage_primary_displayForm_cre_ms'
     config.add_show_field 'mods_name_usage_primary_displayForm_pht_ms', :label => 'Photographer', :link_to_search => 'mods_name_usage_primary_displayForm_pht_ms'
+    config.add_show_field 'mods_name_displayForm_aut_ms', :label => 'Additional Author(s)'
+    config.add_show_field 'mods_name_displayForm_cre_ms', :label => 'Additional Creator(s)'
+    config.add_show_field 'mods_name_displayForm_pht_ms', :label => 'Additional Photographer(s)'
     config.add_show_field 'mods_name_displayForm_crp_ms', :label => 'Correspondent'
     config.add_show_field 'mods_name_displayForm_csl_ms', :label => 'Consultant'
     config.add_show_field 'mods_name_displayForm_ctb_ms', :label => 'Contributor'
@@ -148,21 +154,21 @@ class CatalogController < ApplicationController
     config.add_show_field 'mods_name_displayForm_own_ms', :label => 'Owner'
     config.add_show_field 'mods_name_displayForm_prf_ms', :label => 'Performer'
     config.add_show_field 'mods_name_displayForm_rps_ms', :label => 'Repository'
-    config.add_show_field 'mods_name_displayForm_trc_ms', :label => 'Transcriber'  
+    config.add_show_field 'mods_name_displayForm_trc_ms', :label => 'Transcriber'
     config.add_show_field 'mods_date_display_ms', :label => 'Date'
     config.add_show_field 'mods_format_normalized_ms', :label => 'Format'
     config.add_show_field 'mods_genre_normalized_ms', :label => 'Genre'
     config.add_show_field 'mods_relatedItem_type_host_collection_ms', :label => 'Collection', :link_to_search => 'mods_relatedItem_type_host_collection_ms'
     config.add_show_field 'mods_relatedItem_type_host_series_ms', :label => 'Series', :link_to_search => 'mods_relatedItem_type_host_series_ms'
-    config.add_show_field 'mods_relatedItem_type_host_partOf_ms', :label => 'Subseries'    
+    config.add_show_field 'mods_relatedItem_type_host_partOf_ms', :label => 'Subseries'
     config.add_show_field 'mods_subject_ms', :label => 'Subject', :link_to_search => 'mods_subject_ms'
-    config.add_show_field 'mods_places_ms', :label => 'Place', :link_to_search => 'mods_places_ms'    
+    config.add_show_field 'mods_places_ms', :label => 'Place', :link_to_search => 'mods_places_ms'
     config.add_show_field 'mods_subject_keyword_displayForm_ms', :label => 'Keywords', :link_to_search => 'mods_subject_keyword_displayForm_ms'
     config.add_show_field 'mods_note_ms', :label => 'Note'
     config.add_show_field 'mods_note_type_acquisition_ms', :label => 'Acquisition note'
     config.add_show_field 'mods_note_type_biographical_historical_ms', :label => 'Biographical/Historical note'
     config.add_show_field 'mods_note_type_original_location_ms', :label => 'Original location'
-    config.add_show_field 'mods_note_type_reproduction_ms', :label => 'Reproduction note'    
+    config.add_show_field 'mods_note_type_reproduction_ms', :label => 'Reproduction note'
     config.add_show_field 'mods_physicalDescription_extent_ms', :label => 'Physical Description'
     config.add_show_field 'mods_language_languageTerm_type_text_authority_iso639-2b_ms', :label => 'Language'
     config.add_show_field 'mods_identifier_ms', :label => 'Identifier'
@@ -183,48 +189,48 @@ class CatalogController < ApplicationController
     # The :key is what will be used to identify this BL search field internally,
     # as well as in URLs -- so changing it after deployment may break bookmarked
     # urls.  A display label will be automatically calculated from the :key,
-    # or can be specified manually to be different. 
+    # or can be specified manually to be different.
 
     # This one uses all the defaults set by the solr request handler. Which
     # solr request handler? The one set in config[:default_solr_parameters][:qt],
-    # since we aren't specifying it otherwise. 
-    
+    # since we aren't specifying it otherwise.
+
     config.add_search_field 'all_fields', :label => 'All Fields'
-    
+
 
     # Now we see how to over-ride Solr request handler defaults, in this
     # case for a BL "search field", which is really a dismax aggregate
-    # of Solr search fields. 
-    
+    # of Solr search fields.
+
     config.add_search_field('title') do |field|
-      # solr_parameters hash are sent to Solr as ordinary url query params. 
+      # solr_parameters hash are sent to Solr as ordinary url query params.
       field.solr_parameters = { :'spellcheck.dictionary' => 'title' }
 
       # :solr_local_parameters will be sent using Solr LocalParams
       # syntax, as eg {! qf=$title_qf }. This is neccesary to use
       # Solr parameter de-referencing like $title_qf.
       # See: http://wiki.apache.org/solr/LocalParams
-      field.solr_local_parameters = { 
+      field.solr_local_parameters = {
         :qf => '$title_qf',
         :pf => '$title_pf'
       }
     end
-    
+
     config.add_search_field('author') do |field|
       field.solr_parameters = { :'spellcheck.dictionary' => 'author' }
-      field.solr_local_parameters = { 
+      field.solr_local_parameters = {
         :qf => '$author_qf',
         :pf => '$author_pf'
       }
     end
-    
+
     # Specifying a :qt only to show it's possible, and so our internal automated
-    # tests can test it. In this case it's the same as 
-    # config[:default_solr_parameters][:qt], so isn't actually neccesary. 
+    # tests can test it. In this case it's the same as
+    # config[:default_solr_parameters][:qt], so isn't actually neccesary.
     config.add_search_field('subject') do |field|
       field.solr_parameters = { :'spellcheck.dictionary' => 'subject' }
       field.qt = 'search'
-      field.solr_local_parameters = { 
+      field.solr_local_parameters = {
         :qf => '$subject_qf',
         :pf => '$subject_pf'
       }
@@ -242,7 +248,7 @@ class CatalogController < ApplicationController
     config.add_sort_field 'mods_keydate_sort desc, mods_titleInfo_usage_primary_title_sort asc', :label => 'year'
     config.add_sort_field 'mods_name_usage_primary_sort asc, mods_titleInfo_usage_primary_title_sort asc', :label => 'creator'
     config.add_sort_field 'mods_titleInfo_usage_primary_title_sort asc, mods_keydate_sort desc', :label => 'title'
-    # If there are more than this many search results, no spelling ("did you 
+    # If there are more than this many search results, no spelling ("did you
     # mean") suggestion is offered.
     config.spell_max = 5
   end
@@ -253,4 +259,4 @@ CatalogController.blacklight_config.show.document_actions.delete(:sms)
     false
   end
 
-end 
+end
