f2f20ef0b95a991c03a05387217b1d31f6dac32d
Adam Jazairi <adamjazairi@gmail.com>
2017-09-13 13:57:35 -0400

    Remove initializer files for blacklight and devise

--- config/initializers/blacklight_initializer.rb
+++
@@ -1,8 +0,0 @@
-# A secret token used to encrypt user_id's in the Bookmarks#export callback URL
-# functionality, for example in Refworks export of Bookmarks. In Rails 4, Blacklight
-# will use the application's secret key base instead.
-#
-
-Blacklight.secret_key = 'c64ef93134032b7923bb1ee329ae588c4fb217d407a1110ccc14d8c1113d8dec62d346ed28c7856d5a3d5b7fd68b7434236c730edb54ca57dab7090599fd4fbb'
-Devise.secret_key = '41e70b8d87e3b34591a4862588d11dafebdefa69ce36e5069130f6baf3d3d9c5a0268a7cd3bfe7557454f158a103bffa8652a8f6cf87672ce1c08ff1411ccf44'
-
